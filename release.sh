6c064a2feec69a4ad94e1d4536d1934ba8d4af05
Luigi Agosti <luigi.agosti@gmail.com>
2011-10-18 15:41:17 +0100

    change from sh to bat, I doubt someone on winzoz will run this

--- release.sh
+++ release.sh
@@ -1,6 +1,17 @@
+clear
 mvn release:prepare
+echo Continue?
+read
+clear
 mvn release:perform -Plocalrelease
+echo Continue?
+read
+clear
 mvn android-release:prepare
+echo Continue?
+read
+clear
 git add --all
 git commit -m "[android-manifest-version-update] version changes before starting a new development cycle"
 git push
+echo Build success
