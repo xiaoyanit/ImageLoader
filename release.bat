397439dcb1b7059660a4a8c6681d51a97cb5cdc0
Luigi Agosti <luigi.agosti@gmail.com>
2011-10-18 14:38:18 +0100

    change in the steps of the release

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 mvn release:prepare
-mvn release:perform
+mvn release:perform -Plocalrelease
 mvn android:release
 git add --all
 git commit -m "updating android version before starting a new development cycle"
