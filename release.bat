51139a6a0456bfdea724c82ada5ce8d99fe3ccb4
Luigi Agosti <luigi.agosti@gmail.com>
2011-10-18 14:41:55 +0100

    change in the comment of the release

--- release.bat
+++ release.bat
@@ -2,5 +2,5 @@ mvn release:prepare
 mvn release:perform -Plocalrelease
 mvn android:release
 git add --all
-git commit -m "updating android version before starting a new development cycle"
+git commit -m "[android-manifest-version-update] version changes before starting a new development cycle"
 git push
