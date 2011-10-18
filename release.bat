369d3cb864eb2f6690258765d36e3426cf93344a
Luigi Agosti <luigi.agosti@gmail.com>
2011-10-18 15:08:27 +0100

    stupid error on the bat

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 mvn release:prepare
 mvn release:perform -Plocalrelease
-mvn android:release
+mvn android-release:prepare
 git add --all
 git commit -m "[android-manifest-version-update] version changes before starting a new development cycle"
 git push
