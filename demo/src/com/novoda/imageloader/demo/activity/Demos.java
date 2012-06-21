e56c0de0775377ad5fe19299b408bcc4f0b7776f
Jamie McDonald <jdamcd@gmail.com>
2012-06-21 17:50:50 +0100

    Converted demo app index to ListView.

---
+++ demo/src/com/novoda/imageloader/demo/activity/Demos.java
@@ -0,0 +1,43 @@
+package com.novoda.imageloader.demo.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.novoda.imageloader.demo.R;
+
+public class Demos extends Activity implements OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demos);
+        ListView entries = (ListView) findViewById(R.id.demo_list);
+        entries.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+        case 0:
+            startActivity(new Intent(Demos.this, ImageLongList.class));
+            break;
+        case 1:
+            startActivity(new Intent(Demos.this, LongSmallImageList.class));
+            break;
+        case 2:
+            startActivity(new Intent(Demos.this, BigImages.class));
+            break;
+        case 3:
+            startActivity(new Intent(Demos.this, FromCacheOnly.class));
+            break;
+        case 4:
+            startActivity(new Intent(Demos.this, DirectLoading.class));
+        }
+    }
+
+}
