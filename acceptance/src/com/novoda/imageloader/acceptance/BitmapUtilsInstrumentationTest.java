cbe88ddfc4024a38fa4851555f4da1d2520c6511
friedger <friedger@gmail.com>
2012-09-28 14:18:27 +0200

    some testing needed for #62

---
+++ acceptance/src/com/novoda/imageloader/acceptance/BitmapUtilsInstrumentationTest.java
@@ -0,0 +1,31 @@
+package com.novoda.imageloader.acceptance;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.test.InstrumentationTestCase;
+
+import com.novoda.imageloader.core.bitmap.BitmapUtil;
+
+public class BitmapUtilsInstrumentationTest extends InstrumentationTestCase {
+
+	public BitmapUtilsInstrumentationTest(String name) {
+		super();
+		setName(name);
+	}
+	
+	public void testScaleBitmap(){
+		Bitmap bmOriginal = BitmapFactory.decodeResource(getInstrumentation().getContext().getResources(), R.drawable.icon);
+		assertTrue(bmOriginal.getHeight() <= 72);
+		assertTrue(bmOriginal.getWidth() <= 72);
+		
+		Bitmap bm = new BitmapUtil().decodeResourceBitmapAndScale(getInstrumentation().getContext(), 200, 200, R.drawable.icon, true);		
+		
+		assertEquals(200, bm.getHeight());
+		assertEquals(200, bm.getWidth());
+		
+		bm = new BitmapUtil().decodeResourceBitmapAndScale(getInstrumentation().getContext(), 200, 200, R.drawable.icon, false);		
+		assertEquals(bm, bmOriginal);
+	}
+
+	
+}
