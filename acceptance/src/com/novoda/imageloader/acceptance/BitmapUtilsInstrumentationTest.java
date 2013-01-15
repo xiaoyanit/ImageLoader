0d6017fbf94982527d97585e039c440e7e1718b6
Adam <adam@novoda.com>
2013-01-15 10:48:18 +0000

    bringing across the bitmap util test fixes from master

--- acceptance/src/com/novoda/imageloader/acceptance/BitmapUtilsInstrumentationTest.java
+++ acceptance/src/com/novoda/imageloader/acceptance/BitmapUtilsInstrumentationTest.java
@@ -3,42 +3,37 @@ package com.novoda.imageloader.acceptance;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.test.InstrumentationTestCase;
-import android.test.suitebuilder.annotation.Suppress;
 
 import com.novoda.imageloader.core.bitmap.BitmapUtil;
 
 public class BitmapUtilsInstrumentationTest extends InstrumentationTestCase {
 
-	public BitmapUtilsInstrumentationTest(String name) {
-		super();
-		setName(name);
-	}
+    private final int scaledBitmapSize = 200;
+    private final boolean allowUpSampling = true;
 
-    @Suppress
-	public void testScaleBitmap(){
-		Bitmap bmOriginal = BitmapFactory.decodeResource(getInstrumentation().getContext().getResources(), R.drawable.icon);
-		assertTrue(bmOriginal.getHeight() <= 72);
-		assertTrue(bmOriginal.getWidth() <= 72);
-		
-		Bitmap bm = new BitmapUtil().decodeResourceBitmapAndScale(getInstrumentation().getContext(), 200, 200, R.drawable.icon, true);		
-		
-		assertEquals(200, bm.getHeight());
-		assertEquals(200, bm.getWidth());
-		
-		bm = new BitmapUtil().decodeResourceBitmapAndScale(getInstrumentation().getContext(), 200, 200, R.drawable.icon, false);		
+    public BitmapUtilsInstrumentationTest(String name) {
+        super();
+        setName(name);
+    }
 
-		assertEquals(bmOriginal.getHeight(), bm.getHeight());
-		assertEquals(bmOriginal.getWidth(), bm.getWidth());
+    public void test_bitmaputil_scales_bitmaps_to_specified_size() {
+        Bitmap bmOriginal = BitmapFactory.decodeResource(getInstrumentation().getContext().getResources(), R.drawable.icon);
+        assertFalse(bmOriginal.getHeight() == scaledBitmapSize);
+        assertFalse(bmOriginal.getWidth() == scaledBitmapSize);
 
-		bm = new BitmapUtil().scaleBitmap(bmOriginal, 200, 200, false);		
-		assertEquals(bmOriginal, bm);
-		
-		bm = new BitmapUtil().scaleBitmap(bmOriginal, 200, 200, true);		
-		assertNotSame(bmOriginal, bm);
-		assertEquals(200, bm.getHeight());
-		assertEquals(200, bm.getWidth());
+        Bitmap bm = new BitmapUtil().scaleBitmap(bmOriginal, scaledBitmapSize, scaledBitmapSize, allowUpSampling);
 
-	}
+        assertEquals(scaledBitmapSize, bm.getHeight());
+        assertEquals(scaledBitmapSize, bm.getWidth());
+    }
 
-	
+    public void test_bitmaputil_creates_bitmaps__of_a_specified_size() {
+        Bitmap bm = new BitmapUtil().decodeResourceBitmapAndScale(getInstrumentation().getContext(),
+                scaledBitmapSize, scaledBitmapSize, R.drawable.icon, allowUpSampling);
+
+        assertTrue(bm.getHeight() == scaledBitmapSize);
+        assertTrue(bm.getWidth() == scaledBitmapSize);
+    }
+
 }
+
