6c21b4a280aed8c9047e75a5f18ac5f06cb7b271
Peter <peter.budo@gmail.com>
2013-08-13 11:59:53 +0100

    Fixed checkstyle reports of WhitespaceAroundCheck

--- core/src/main/java/com/novoda/imageloader/core/loader/util/LoaderTask.java
+++ core/src/main/java/com/novoda/imageloader/core/loader/util/LoaderTask.java
@@ -49,7 +49,7 @@ public class LoaderTask extends AsyncTask<String, Void, Bitmap> {
         this.imageWrapper = imageWrapper;
         this.loaderSettings = loaderSettings;
         this.onImageLoadedListener = onImageLoadedListener;
-        if(imageWrapper != null){
+        if (imageWrapper != null) {
             setTagInformation(imageWrapper);
         }
     }
