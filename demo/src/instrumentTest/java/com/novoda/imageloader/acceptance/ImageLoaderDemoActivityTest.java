341894e7c9736fc8638d877fa254564e2d723cfb
Carl-Gustaf Harroch <carl@novoda.com>
2013-10-30 12:54:39 +0000

    made gradle push

--- demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageLoaderDemoActivityTest.java
+++ demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageLoaderDemoActivityTest.java
@@ -5,6 +5,7 @@ import android.widget.AbsListView;
 
 import com.jayway.android.robotium.solo.Solo;
 import com.novoda.imageloader.demo.activity.ImageLongList;
+import com.novoda.imageloader.demo.R;
 
 public class ImageLoaderDemoActivityTest extends ActivityInstrumentationTestCase2<ImageLongList> {
 
@@ -26,8 +27,7 @@ public class ImageLoaderDemoActivityTest extends ActivityInstrumentationTestCase
     }
 
     public void testScrollingThroughList() {
-        solo.waitForActivity(solo.getCurrentActivity().toString());
-        AbsListView list = solo.getCurrentListViews().get(LIST_INDEX);
+        AbsListView list = (AbsListView) solo.getView(R.id.list_view);
         
         for (int i = 0; i < Math.max(2, list.getCount()); i++) {
             solo.scrollDown();
