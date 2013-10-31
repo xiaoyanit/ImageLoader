3dba9173b5e915825bdb88f67e402fde697c479f
Ataul Munim <timberwo7ves@gmail.com>
2013-10-31 11:47:23 +0000

    add logging

--- demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageLoaderDemoActivityTest.java
+++ demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageLoaderDemoActivityTest.java
@@ -1,11 +1,12 @@
 package com.novoda.imageloader.acceptance;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.AbsListView;
 
 import com.jayway.android.robotium.solo.Solo;
-import com.novoda.imageloader.demo.activity.ImageLongList;
 import com.novoda.imageloader.demo.R;
+import com.novoda.imageloader.demo.activity.ImageLongList;
 
 public class ImageLoaderDemoActivityTest extends ActivityInstrumentationTestCase2<ImageLongList> {
     private Solo solo;
@@ -24,15 +25,21 @@ public class ImageLoaderDemoActivityTest extends ActivityInstrumentationTestCase
         assertNotNull(solo);
     }
 
-    public void testScrollingDownTheList() {
+    public void testScrollingDownTheEntireList() {
         AbsListView list = (AbsListView) solo.getView(R.id.list_view);
         solo.scrollListToTop(list);
-        
+
         boolean shouldScroll = true;
         while (shouldScroll) {
             shouldScroll = solo.scrollDownList(list);
         }
-        
+
+        int lastVisiblePosition = list.getLastVisiblePosition();
+        int selectedPosition = list.getSelectedItemPosition();
+
+        Log.d("IMAGELOADER", "adapter count: " + list.getAdapter().getCount());
+        Log.d("IMAGELOADER", "lastVis: " + lastVisiblePosition);
+        Log.d("IMAGELOADER", "selected: " + selectedPosition);
         assertEquals(list.getLastVisiblePosition(), list.getSelectedItemPosition());
     }
 }
