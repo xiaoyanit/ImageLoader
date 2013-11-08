3fe986b9a3f264fbcb6f55feebce3cd327d58387
Ataul Munim <timberwo7ves@gmail.com>
2013-11-08 17:51:51 +0000

    Refactors BitmapRetrieverTest.cacheImage (inc rename)

--- core/src/test/java/com/novoda/imageloader/core/loader/util/BitmapRetrieverTest.java
+++ core/src/test/java/com/novoda/imageloader/core/loader/util/BitmapRetrieverTest.java
@@ -8,7 +8,6 @@ import com.novoda.imageloader.core.bitmap.BitmapUtil;
 import com.novoda.imageloader.core.cache.CacheManager;
 import com.novoda.imageloader.core.file.FileManager;
 import com.novoda.imageloader.core.model.ImageTag;
-import com.novoda.imageloader.core.model.ImageWrapper;
 import com.novoda.imageloader.core.network.NetworkManager;
 
 import java.io.File;
@@ -18,45 +17,47 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class BitmapRetrieverTest {
+    final File fileCachedImage = mock(File.class);
+    final CacheManager cacheManager = mock(CacheManager.class);
+    final static String IMAGE_URL = "http://king.com/img.png";
+    static final int WIDTH = 100;
+    static final int HEIGHT = 100;
 
     @Test
-    public void testCacheImage() {
+    public void testReferenceToCachedImageIsRemovedWhenBitmapRetrievalFails() {
+        BitmapRetriever retriever = getRetrieverForCachedImageFile();
 
-        File file = mock(File.class);
-        when(file.exists()).thenReturn(true);
-        FileManager fm = mock(FileManager.class);
-        when(fm.getFile("http://king.com/img.png", 100, 100)).thenReturn(file);
-
-        final BitmapUtil bmUtil = mock(BitmapUtil.class);
-        when(bmUtil.decodeFile(file, 100, 100)).thenReturn(null);
-        LoaderSettings loaderSettings = new LoaderSettings() {
-            public BitmapUtil getBitmapUtil() {
-                return bmUtil;
-            }
-        };
-
-        NetworkManager nm = mock(NetworkManager.class);
-
-        loaderSettings.setNetworkManager(nm);
-        loaderSettings.setFileManager(fm);
-
-        CacheManager cache = mock(CacheManager.class);
-        loaderSettings.setCacheManager(cache);
-
-        ImageTag imageTag = new ImageTag("http://king.com/img.png", 0, 0, 100, 100);
-        ImageView imageView = createImageView(imageTag);
-        ImageWrapper imageWrapper = new ImageWrapper(imageView);
-
-        BitmapRetriever retriever = new BitmapRetriever("http://king.com/img.png", file, 100, 100, 0, false, true, imageView, loaderSettings, mock(Context.class));
         retriever.getBitmap();
 
-        // file decode failed, therefore nothing in cache
-        verify(cache, atLeastOnce()).remove("http://king.com/img.png");
-        verify(file, atLeastOnce()).delete();
-
+        verify(cacheManager, atLeastOnce()).remove(IMAGE_URL);
+        verify(fileCachedImage, atLeastOnce()).delete();
     }
 
-    private ImageView createImageView(ImageTag tag){
+    private BitmapRetriever getRetrieverForCachedImageFile() {
+        final FileManager fileManager = mock(FileManager.class);
+        final BitmapUtil bitmapUtil = mock(BitmapUtil.class);
+        final NetworkManager networkManager = mock(NetworkManager.class);
+
+        when(fileCachedImage.exists()).thenReturn(true);
+        when(fileManager.getFile(IMAGE_URL, WIDTH, HEIGHT)).thenReturn(fileCachedImage);
+        when(bitmapUtil.decodeFile(fileCachedImage, WIDTH, HEIGHT)).thenReturn(null);
+
+        LoaderSettings loaderSettings = new LoaderSettings() {
+            public BitmapUtil getBitmapUtil() {
+                return bitmapUtil;
+            }
+        };
+        loaderSettings.setNetworkManager(networkManager);
+        loaderSettings.setFileManager(fileManager);
+        loaderSettings.setCacheManager(cacheManager);
+
+        ImageTag imageTag = new ImageTag(IMAGE_URL, 0, 0, WIDTH, HEIGHT);
+        ImageView imageView = createImageView(imageTag);
+
+        return new BitmapRetriever(IMAGE_URL, fileCachedImage, WIDTH, HEIGHT, 0, false, true, imageView, loaderSettings, mock(Context.class));
+    }
+
+    private ImageView createImageView(ImageTag tag) {
         ImageView imageView = mock(ImageView.class);
         when(imageView.getTag()).thenReturn(tag);
         return imageView;
