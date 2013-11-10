11dae18be8a22517160db8ad7330a65677894e68
Ataul Munim <timberwo7ves@gmail.com>
2013-11-10 17:06:42 +0000

    Adds LruBitmapCache to LoaderSettings used in testCacheImage

--- demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageManagerInstrumentationTest.java
+++ demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageManagerInstrumentationTest.java
@@ -4,6 +4,8 @@ import android.test.InstrumentationTestCase;
 
 import com.novoda.imageloader.core.ImageManager;
 import com.novoda.imageloader.core.LoaderSettings;
+import com.novoda.imageloader.core.cache.CacheManager;
+import com.novoda.imageloader.core.cache.LruBitmapCache;
 
 /**
  * Created with IntelliJ IDEA. User: friedger Date: 29.10.12 Time: 21:59 To change this template use File | Settings | File Templates.
@@ -16,12 +18,15 @@ public class ImageManagerInstrumentationTest extends InstrumentationTestCase {
     }
 
     public void testCacheImage() {
-        LoaderSettings settings = new LoaderSettings.SettingsBuilder().build(getInstrumentation().getContext());
+        CacheManager cache = new LruBitmapCache(getInstrumentation().getContext());
+        LoaderSettings settings = new LoaderSettings.SettingsBuilder().withCacheManager(cache).build(getInstrumentation().getContext());
         ImageManager imageManager = new ImageManager(getInstrumentation().getTargetContext(), settings);
 
         String url = "http://imgur.com/5FirD.png";
-        imageManager.cacheImage(url, 64, 64);
+        int width = 651;
+        int height = 481;
+        imageManager.cacheImage(url, width, height);
 
-        assertTrue(imageManager.getCacheManager().get(url, 64, 64) != null);
+        assertTrue(imageManager.getCacheManager().get(url, width, height) != null);
     }
 }
