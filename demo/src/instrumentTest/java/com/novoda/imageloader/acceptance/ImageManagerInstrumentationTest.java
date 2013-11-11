f762459e35d05e345e0c5ddbdb35e92fd5681279
Ataul Munim <timberwo7ves@gmail.com>
2013-11-11 11:19:00 +0000

    Implements NetworkManager instead of FileManager to fix error (WIP).
    - The error caused on instrumentation test devices was due to lack of network connection
    - we should emulate the networkmanager, not the filemanager
    - :x: the tests don't complete - it's stuck on
    
        > Building > :demo:connectedInstrumentTest (no errors)

--- demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageManagerInstrumentationTest.java
+++ demo/src/instrumentTest/java/com/novoda/imageloader/acceptance/ImageManagerInstrumentationTest.java
@@ -1,11 +1,10 @@
 package com.novoda.imageloader.acceptance;
 
-import android.graphics.Bitmap;
 import android.test.InstrumentationTestCase;
 
 import com.novoda.imageloader.core.ImageManager;
 import com.novoda.imageloader.core.LoaderSettings;
-import com.novoda.imageloader.core.file.FileManager;
+import com.novoda.imageloader.core.network.NetworkManager;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,6 +12,9 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ImageManagerInstrumentationTest extends InstrumentationTestCase {
+    private static final String IMAGE_URL = "http://thisurldontmatter.co.whaat";
+    private static final int WIDTH = 0;
+    private static final int HEIGHT = 0;
 
     public ImageManagerInstrumentationTest(String name) {
         super();
@@ -20,67 +22,60 @@ public class ImageManagerInstrumentationTest extends InstrumentationTestCase {
     }
 
     public void testAfterImageIsCached_imageIsRetrievableFromCache() throws IOException {
-        final File file = createImageFile();
+        ImageManager imageManager = createImageManagerBackedByCustomNetworkManager();
 
-        ImageManager imageManager = createImageManagerBackedByFile(file);
+        imageManager.cacheImage(IMAGE_URL, WIDTH, HEIGHT);
 
-        String url = "http://any_url_should_do_here.com";
-        imageManager.cacheImage(url, 64, 64);
-
-        assertNotNull("image should be in cache!", imageManager.getCacheManager().get(url, 64, 64));
+        assertNotNull("image should be in cache!", imageManager.getCacheManager().get(IMAGE_URL, WIDTH, HEIGHT));
     }
 
-    private ImageManager createImageManagerBackedByFile(File file) {
+    private ImageManager createImageManagerBackedByCustomNetworkManager() {
         LoaderSettings settings = new LoaderSettings.SettingsBuilder()
-                .withFileManager(new SingleFileManager(file))
+                .withNetworkManager(new SingleUrlNetworkManager())
                 .build(getInstrumentation().getTargetContext());
         return new ImageManager(getInstrumentation().getTargetContext(), settings);
     }
 
-    private File createImageFile() throws IOException {
-        InputStream imageStream = getInstrumentation().getContext().getAssets().open("ic_launcher.png");
-        int read;
-        final File file = new File(getInstrumentation().getTargetContext().getCacheDir(), "image.png");
-        FileOutputStream outputStream = new FileOutputStream(file);
-        while ((read = imageStream.read()) != -1) {
-            outputStream.write(read);
-        }
-        imageStream.close();
-        outputStream.close();
-        return file;
-    }
-
-    private static class SingleFileManager implements FileManager {
-        private final File file;
-
-        public SingleFileManager(File file) {
-            this.file = file;
+    private class SingleUrlNetworkManager implements NetworkManager {
+        @Override
+        public void retrieveImage(String url, File file) {
+            if (!url.equals(IMAGE_URL)) {
+                return;
+            }
+            InputStream imageStream = retrieveInputStream(url);
+            file = createImageFileFromStream(file, imageStream);
         }
 
         @Override
-        public void clean() {
+        public InputStream retrieveInputStream(String url) {
+            try {
+                return getInstrumentation().getContext().getAssets().open("ic_launcher.png");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
 
-        @Override
-        public void cleanOldFiles() {
-        }
-
-        @Override
-        public String getFilePath(String url) {
-            return null;
-        }
-
-        @Override
-        public File getFile(String url) {
-            return file;
-        }
-
-        @Override
-        public void saveBitmap(String fileName, Bitmap b, int width, int height) {
-        }
-
-        @Override
-        public File getFile(String url, int width, int height) {
+        private File createImageFileFromStream(File file, InputStream imageStream) {
+            if (imageStream == null) {
+                return null;
+            }
+            int read;
+            FileOutputStream outputStream = null;
+            try {
+                outputStream = new FileOutputStream(file);
+                while ((read = imageStream.read()) != -1) {
+                    outputStream.write(read);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    imageStream.close();
+                    outputStream.close();
+                } catch (IOException ignore) {
+                }
+            }
             return file;
         }
     }
